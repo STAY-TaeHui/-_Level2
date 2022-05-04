07fafbd98fa9c2280cc71593113abd8b92842a42
root <root@thseo.spectra.co.kr>
2022-05-04 15:20:55 +0900

    solved

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import com.company.문자열압축.Solution;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Solution solution = new Solution();
+        int result;
+        result = solution.solution("abcabcdede");
+        System.out.println("result : " + result);
+    }
+}
