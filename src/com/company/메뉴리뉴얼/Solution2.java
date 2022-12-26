935bbf3aca7e8f78e7b6d28d31ae7ac3dd88d519
STAY-TaeHui <masta95@naver.com>
2022-12-26 22:57:05 +0900

    메뉴리뉴얼 - 참고해서 다시 코딩중...

--- src/com/company/메뉴리뉴얼/Solution2.java
+++ src/com/company/메뉴리뉴얼/Solution2.java
@@ -1,6 +1,32 @@
 package com.company.메뉴리뉴얼;
 //Solution 클래스를 본 후에 코딩
 
-public class Solution2 {
+import java.util.ArrayList;
+import java.util.Arrays;
 
+public class Solution2 {
+    public static void main(String[] args) {
+        solution(new String[]{"XYZ", "XWY", "WXA"},new int[]{2,3,4});
+    }
+    public static ArrayList<String> solution(String[] orders, int[] course) {
+        ArrayList<String> answer = new ArrayList<>();
+
+        for(int i=0; i<orders.length; i++){
+            char[] chars = orders[i].toCharArray();
+            Arrays.sort(chars);
+            orders[i] = String.valueOf(chars);
+        }
+
+        for(int i=0; i<course.length; i++) {
+            StringBuilder sb = new StringBuilder();
+
+            for (int j = 0; j < orders.length; j++) {
+                if (course[i] <= orders[j].length()) {
+                    combi(orders[i], )
+
+                }
+            }
+        }
+        return answer;
+    }
 }
