298fddb87c54437fd4bd118f75e1c68a8ea4f2b2
STAY-TaeHui <masta95@naver.com>
2022-12-29 22:55:29 +0900

    카펫 - 시작

---
+++ src/com/company/카펫/Main.java
@@ -0,0 +1,13 @@
+package com.company.카펫;
+//https://school.programmers.co.kr/learn/courses/30/lessons/42842
+public class Main {
+    public static void main(String[] args) {
+        Solution.solution(10,2);
+    }
+}
+class Solution {
+    public static int[] solution(int brown, int yellow) {
+        int[] answer = {};
+        return answer;
+    }
+}
