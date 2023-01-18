5973c3975f26057d0754ed90816131d1e0c3ebe8
서태희 <masta95@naver.com>
2023-01-18 15:55:17 +0900

    숫자의표현 - 효율테스트 실패

---
+++ src/com/company/숫자의표현/Main.java
@@ -0,0 +1,41 @@
+package com.company.숫자의표현;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/12924
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Solution.solution(15);
+    }
+
+}
+/*
+1 + 2 + 3 + 4 + 5 = 15
+4 + 5 + 6 = 15
+7 + 8 = 15
+15 = 15
+1,3,5,15
+1,4,16
+
+8+9 = 17
+1,17
+* */
+class Solution {
+    public static int solution(int n) {
+        int answer = 1;
+        int sum=0;
+
+        for(int i=1; i<=n/2; i++){
+            for(int j=i; j<=n/2+1; j++){
+                sum+=j;
+                if(sum==n){
+                    answer++;
+                    break;
+                }
+            }
+            sum=0;
+        }
+        System.out.println(answer);
+        return answer;
+    }
+}
