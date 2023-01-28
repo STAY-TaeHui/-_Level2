af1cb993d83ef6bef8bf53045f8e4870714d552d
STAY-TaeHui <masta95@naver.com>
2023-01-28 19:13:46 +0900

    숫자의표현 - 조건추가하여 완료

--- src/com/company/숫자의표현/Main.java
+++ src/com/company/숫자의표현/Main.java
@@ -1,5 +1,9 @@
 package com.company.숫자의표현;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 //https://school.programmers.co.kr/learn/courses/30/lessons/12924
 public class Main
 {
@@ -26,7 +30,7 @@ class Solution {
         int sum=0;
 
         for(int i=1; i<=n/2; i++){
-            for(int j=i; j<=n/2+1; j++){
+            for(int j=i; sum<n; j++){
                 sum+=j;
                 if(sum==n){
                     answer++;
