2b0db0af63497eff0b73ba4b256dde728724cf7c
STAY-TaeHui <masta95@naver.com>
2023-03-02 21:28:50 +0900

    예상대진표 - 시작

---
+++ src/com/company/예상대진표/Main.java
@@ -0,0 +1,19 @@
+package com.company.예상대진표;
+//https://school.programmers.co.kr/learn/courses/30/lessons/12985
+public class Main {
+    public static void main(String[] args) {
+        Solution.solution(8,4,7);
+    }
+}
+class Solution
+{
+    public static int solution(int n, int a, int b)
+    {
+        int answer = 0;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
