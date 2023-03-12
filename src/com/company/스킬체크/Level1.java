b26d29f84a02564379132f15b4bd45bcf5beed42
STAY-TaeHui <masta95@naver.com>
2023-03-12 16:55:45 +0900

    스킬체크 Lv.1 성공

---
+++ src/com/company/스킬체크/Level1.java
@@ -0,0 +1,62 @@
+package com.company.스킬체크;
+
+import java.util.Stack;
+
+public class Level1 {
+    public static void main(String[] args) {
+        Solution.solution("1S2D*3T");
+    }
+}
+class Solution{
+    public static int solution(String dartResult) {
+        int answer = 0;
+        Stack<Integer> st = new Stack<>();
+        char[] chars = dartResult.toCharArray();
+        int point = 0;
+        for(char c : chars){
+            int cToInt = Character.getNumericValue(c);
+            if (0 <= cToInt &&cToInt <= 10) {
+                point = cToInt;
+                continue;
+            }
+            else{
+                switch (c){
+                    case 'S':{//single
+                        int pow = (int) Math.pow(point, 1);
+                        st.add(pow);
+                        continue;
+
+                    }
+                    case 'D':{//double
+                        int pow = (int) Math.pow(point,2);
+                        st.add(pow);
+                        continue;
+                    }
+                    case 'T':{//triple
+                        int pow = (int) Math.pow(point,3);
+                        st.add(pow);
+                        continue;
+                    }
+                    case '*':{
+                        int temp = st.pop()*2;
+                        if(!st.isEmpty())
+                            temp +=st.pop()*2;
+                        st.add(temp);
+                    }
+                    case '#':{
+                        int temp = st.pop()*(-1);
+                        if(!st.isEmpty())
+                        temp += st.pop()*(-1);
+                        st.add(temp);
+                    }
+                }
+            }
+        }
+        for(int a : st){
+            System.out.println(a);
+            answer+=a;
+        }
+        System.out.println(answer);
+        return answer;
+    }
+}
---
+++ src/com/company/스킬체크/Level1_2.java
@@ -0,0 +1,50 @@
+package com.company.스킬체크;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class Level1_2 {
+    public static void main(String[] args) {
+//        Solution2.solution(118372);
+        Solution3.solution("banana");
+    }
+}
+class Solution2{// 숫자를 desc 정렬
+    public static long solution(long n) {
+        long answer = 0;
+        String nStr = n+"";
+        char[] chars = nStr.toCharArray();
+        Arrays.sort(chars);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(chars);
+        sb.reverse();
+        System.out.println(sb);
+        answer = Long.parseLong(sb.toString());
+        return answer;
+    }
+}
+class Solution3{//banana --> 처음인 문자는 -1, 나왔던 문자는 이전 문자의 index
+    public static int[] solution(String s) {
+        int[] answer = {};
+        answer= new int[s.length()];
+        StringBuilder sb = new StringBuilder();
+        Stack<Character> st = new Stack<>();
+
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+
+            if(!st.contains(c)) {
+                st.add(c);
+                answer[i] = -1;
+            }
+            else{
+                int search = st.search(c);
+                st.add(c);
+                answer[i] = search;
+            }
+
+        }
+        return answer;
+    }
+}
